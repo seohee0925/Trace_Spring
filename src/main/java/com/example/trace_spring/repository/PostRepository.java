a283f3dff32b3c6724314d67b91e14b78569509b
JyngJ <kaijjy@kaist.ac.kr>
2024-01-07 17:55:42 +0900

    feature: 포스팅 구현#1 Json받아서 DB 등록 성공

---
+++ src/main/java/com/example/trace_spring/repository/PostRepository.java
@@ -0,0 +1,16 @@
+package com.example.trace_spring.repository;
+
+import com.example.trace_spring.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findPostByEmail(String email);
+
+//    // 위치 기반 검색을 위한 메소드 (예시)
+//    @Query("SELECT p FROM Post p WHERE ...") // 위치 검색을 위한 쿼리 구현
+//    List<Post> findNearbyPosts(Double latitude, Double longitude, Double radius);
+
+    // 기타 필요한 메소드 구현
+}
