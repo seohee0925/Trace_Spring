dc03bbe50f95fd1bcc867233d897ad076596a99a
JyngJ <kaijjy@kaist.ac.kr>
2024-01-09 01:23:42 +0900

    feature: 포스팅 구현#4 지도 보이는 뷰에서 포스팅 값 읽어서 좌표 찍기

--- src/main/java/com/example/trace_spring/repository/PostRepository.java
+++ src/main/java/com/example/trace_spring/repository/PostRepository.java
@@ -3,14 +3,18 @@ package com.example.trace_spring.repository;
 import com.example.trace_spring.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findPostByEmail(String email);
 
-//    // 위치 기반 검색을 위한 메소드 (예시)
-//    @Query("SELECT p FROM Post p WHERE ...") // 위치 검색을 위한 쿼리 구현
-//    List<Post> findNearbyPosts(Double latitude, Double longitude, Double radius);
-
     // 기타 필요한 메소드 구현
+
+    @Query("SELECT p FROM Post p WHERE p.latitude >= :southWestLat AND p.latitude <= :northEastLat AND p.longitude >= :southWestLng AND p.longitude <= :northEastLng")
+    List<Post> findPostsInArea(@Param("southWestLat") double southWestLat,
+                               @Param("southWestLng") double southWestLng,
+                               @Param("northEastLat") double northEastLat,
+                               @Param("northEastLng") double northEastLng);
 }
