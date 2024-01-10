49cf26fecb64b864225037269fab34802387321e
윤서희 <appleid21@sookmyung.ac.kr>
2024-01-10 13:07:41 +0900

    Fix : KST 반영

--- src/main/java/com/example/trace_spring/controller/PostController.java
+++ src/main/java/com/example/trace_spring/controller/PostController.java
@@ -7,9 +7,10 @@ import com.example.trace_spring.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 @RestController
 @RequestMapping("/posts")
@@ -37,8 +38,36 @@ public class PostController {
             return ResponseEntity.ok(response);
         }
 
-        return ResponseEntity.ok(posts);
+        List<Map<String, Object>> postsResponse = new ArrayList<>();
+        ZoneId serverZone = TimeZone.getDefault().toZoneId(); // 서버의 기본 타임존 가져오기
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME; // ISO 8601 형식 포맷터
+
+        for (Post post : posts) {
+            Map<String, Object> postMap = new HashMap<>();
+            postMap.put("id", post.getId());
+            postMap.put("content", post.getContent());
+            postMap.put("latitude", post.getLatitude());
+            postMap.put("longitude", post.getLongitude());
+            postMap.put("email", post.getEmail());
+            postMap.put("image", post.getImage());
+            postMap.put("imageExtra", post.getImageExtra());
+            postMap.put("address", post.getAddress());
+
+            // 시간대 변환
+            ZonedDateTime zonedDateTime = post.getCreatedDate().toInstant().atZone(serverZone);
+            ZonedDateTime userZoneTime = zonedDateTime.withZoneSameInstant(ZoneId.of("Asia/Seoul")); // 사용자의 타임존 설정
+
+            // ISO 8601 형식의 문자열로 변환하여 넣기
+            String formattedDateTime = userZoneTime.format(formatter);
+            postMap.put("createdDate", formattedDateTime);
+
+            postsResponse.add(postMap);
+        }
+
+        return ResponseEntity.ok(postsResponse);
     }
+
+
 
     @GetMapping("/inArea")
     public ResponseEntity<Object> getPostsInArea(
